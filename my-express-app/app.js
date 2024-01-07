08757afe8ab6533d7b67fdea0da074646efee4cb
YousefMoussa <129340216+YousefMoussa@users.noreply.github.com>
2024-01-06 20:26:56 -0700

    App V3

--- my-express-app/app.js
+++ my-express-app/app.js
@@ -35,16 +35,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.post('/upload', upload.single('pdfFile'), (req, res) => {
   // 'pdfFile' is the name attribute in your HTML file input
   console.log(req.file); // Information about the uploaded file
-  res.send('File uploaded successfully');
+  //res.send('File uploaded successfully');
+  res.redirect('/interview.html');
 });
 
 // Handle form submission POST request
 app.post('/submit-form', (req, res) => {
-    const jobTitle = req.body.Jobtitle;
-    const jobDescription = req.body.JobDiscription;
+    const jobtitle = req.body.jobtitle;
+    const description = req.body.description;
 
     // Create a string to save to the .txt file
-    const content = `Job_Title = \'${jobTitle}\'\nJob_Description = \'${jobDescription}\'`;
+    const content = `Job_Title = \'${jobtitle}\'\nJob_Description = \'${description}\'`;
 
     // Write to a .txt file
     fs.writeFile('C:/Hacked_Jan6/Metamorphosis/DetailsAndRole.py', content, (err) => {
@@ -52,7 +53,8 @@ app.post('/submit-form', (req, res) => {
             console.error(err);
             return res.send('An error occurred while writing to the file.');
         }
-        res.send('Form data received and file written.');
+        //res.send('Form data received and file written.');
+        res.redirect('/resume.html');
     });
 });
 
